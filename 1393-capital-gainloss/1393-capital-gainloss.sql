d6814eee76d8eeca3a052eeb94693587b5fe0281
Alok Sharma <2k23.csai2313910@gmail.com>
2026-08-25 16:05:32 +0530

    Time: 648 ms (22.21%), Space: 0B (100%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.sql
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,19 +1,5 @@
 # Write your MySQL query statement below
-with buy as (
-    select stock_name , sum(price) as spent
-    from stocks
-    where operation = "buy"
-    group by stock_name
-),
-
-sell as (
-    select stock_name , sum(price) as gained
-    from stocks
-    where operation = "sell"
-    group by stock_name
-)
-
-select s.stock_name , s.gained - b.spent as capital_gain_loss
-from buy b join sell s
+select b.stock_name, s.gained - b.spent  as  capital_gain_loss
+from (select stock_name, sum(price) as spent from stocks where operation = "buy" group by stock_name) b join (select stock_name, sum(price) as gained from stocks where operation = "sell" group by stock_name) s
 on b.stock_name = s.stock_name
-group by s.stock_name
+group by stock_name; 
